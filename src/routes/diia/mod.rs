722a60ef73c3d708c95309ebae2d8585b1554f4a
Nazar Demchuk <nazar.demchvk@gmail.com>
2025-01-08 21:50:08 +0200

    Added logging arbitratry JSON for Diia data packages

--- src/routes/diia/mod.rs
+++ src/routes/diia/mod.rs
@@ -1,5 +1,6 @@
 use axum::extract::{State, Json};
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use crate::commands::subcommands::server::ServerState;
 
 #[derive(Deserialize)]
@@ -15,6 +16,8 @@ pub struct DiiaResponse {
 
 pub async fn diia(
     State(_state): State<ServerState>,
+    Json(payload): Json<Value>,
 ) -> Json<DiiaResponse> {
+    println!("Received payload: {}", payload);
     Json(DiiaResponse { success: true })
 }
