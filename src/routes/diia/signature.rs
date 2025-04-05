234549eddd36c8bf1af79cf2a56c070ed35196a6
Nazar Demchuk <nazar.demchvk@gmail.com>
2025-04-05 21:44:38 +0300

    Added decryption for the signed hash of the file in Diia signature handler

--- src/routes/diia/signature.rs
+++ src/routes/diia/signature.rs
@@ -1,12 +1,11 @@
-use std::{str::from_utf8, sync::Arc};
+use std::str::from_utf8;
 
 use crate::{
-    commands::server::ServerState, routes::agreement::get_sign_link::SignHashRequestId, utils::{
-        cache::{AgreementProposalKey, AgreementProposalValue}, eusign::decrypt_customer_data, server_error::ServerError
+    commands::server::ServerState, utils::{
+        eusign::decrypt_customer_data, server_error::ServerError
     }
 };
 use axum::extract::{Json, Multipart, State};
-use moka::ops::compute::Op;
 use serde::Serialize;
 use tracing::info;
 
@@ -75,58 +74,58 @@ pub async fn handler(
     // TODO
     // 5. Updating the cache (changing tenant_signed or landlord_singed)
 
-    let request_id = SignHashRequestId {
-        tenant_id: "1".into(),
-        landlord_id: "2".into(),
-        signed_by: "1".into(),
-        seed: uuid::uuid!("12345678-1234-5678-1234-567812345678"),
-    };
-    let request_id = serde_json::to_string(&request_id)?;
-    let SignHashRequestId {
-        tenant_id,
-        landlord_id: _,
-        signed_by,
-        ..
-    } = serde_json::from_str(&request_id)?;
+    // let request_id = SignHashRequestId {
+    //     tenant_id: "1".into(),
+    //     landlord_id: "2".into(),
+    //     signed_by: "1".into(),
+    //     seed: uuid::uuid!("12345678-1234-5678-1234-567812345678"),
+    // };
+    // let request_id = serde_json::to_string(&request_id)?;
+    // let SignHashRequestId {
+    //     tenant_id,
+    //     landlord_id: _,
+    //     signed_by,
+    //     ..
+    // } = serde_json::from_str(&request_id)?;
 
-    state
-        .cache
-        .entry(AgreementProposalKey {
-            tenant_id: tenant_id.clone(),
-            landlord_id: tenant_id.clone(),
-        })
-        .and_compute_with(|entry| {
-            let op = match entry {
-                Some(entry) => {
-                    if signed_by == tenant_id {
-                        Op::Put(Arc::new(AgreementProposalValue {
-                            tenant_signed: true,
-                            ..*entry.into_value().as_ref()
-                        }))
-                    } else {
-                        Op::Put(Arc::new(AgreementProposalValue {
-                            landlord_signed: true,
-                            ..*entry.into_value().as_ref()
-                        }))
-                    }
-                }
-                None => {
-                    if signed_by == tenant_id {
-                        Op::Put(Arc::new(AgreementProposalValue {
-                            tenant_signed: true,
-                            ..Default::default()
-                        }))
-                    } else {
-                        Op::Put(Arc::new(AgreementProposalValue {
-                            landlord_signed: true,
-                            ..Default::default()
-                        }))
-                    }
-                },
-            };
+    // state
+    //     .cache
+    //     .entry(AgreementProposalKey {
+    //         tenant_id: tenant_id.clone(),
+    //         landlord_id: tenant_id.clone(),
+    //     })
+    //     .and_compute_with(|entry| {
+    //         let op = match entry {
+    //             Some(entry) => {
+    //                 if signed_by == tenant_id {
+    //                     Op::Put(Arc::new(AgreementProposalValue {
+    //                         tenant_signed: true,
+    //                         ..*entry.into_value().as_ref()
+    //                     }))
+    //                 } else {
+    //                     Op::Put(Arc::new(AgreementProposalValue {
+    //                         landlord_signed: true,
+    //                         ..*entry.into_value().as_ref()
+    //                     }))
+    //                 }
+    //             }
+    //             None => {
+    //                 if signed_by == tenant_id {
+    //                     Op::Put(Arc::new(AgreementProposalValue {
+    //                         tenant_signed: true,
+    //                         ..Default::default()
+    //                     }))
+    //                 } else {
+    //                     Op::Put(Arc::new(AgreementProposalValue {
+    //                         landlord_signed: true,
+    //                         ..Default::default()
+    //                     }))
+    //                 }
+    //             },
+    //         };
 
-            std::future::ready(op)
-        }).await;
+    //         std::future::ready(op)
+    //     }).await;
 
     Ok(Json(Response { success: true }))
 }
